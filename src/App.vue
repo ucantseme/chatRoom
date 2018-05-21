a61a7ed1d7ef6d13d705d54ef48fedcec5b015b6
siang <weishang@4hotel.tw>
2018-05-21 21:55:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">HelloWorld</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/HelloWorld2">messageBorad</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="borad container">
+      <div class="input-group my-5">
+        <input type="text" class="form-control" placeholder="請輸入留言內容" v-model="message">
+        <div class="input-group-append">
+        <button class="btn btn-outline-primary" @click="setMessage" type="button">Enter</button>
+        </div>
+      </div>
+      <div class="media my-3" v-for="text in messages" >
+        <img v-if="text.username != username" class="align-self-start mr-3" src="https://picsum.photos/80/100">
+        <div class="media-body">
+          <h3>{{text.username}}</h3>
+          <p>{{text.message}}</p>
+        </div>
+        <img v-if="text.username == username" class="align-self-start mr-3" src="https://picsum.photos/80/100?image=0">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: 'VueCli',
+      message: '',
+      messages: [
+        {
+          username: 'VueCli',
+          message: 'This is Vue-cli!',
+        },
+        {
+          username: 'Justin',
+          message: 'Hello!',
+        },
+      ]
+    }
+  },
+  methods:{
+    setMessage(){
+      let vm = this
+      vm.messages.unshift({
+        username: vm.username,
+        message: vm.message,
+      })
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
