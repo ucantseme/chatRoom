510d42baef292f09ee717f6d67be7a6a4a4b13a9
siang <weishang@4hotel.tw>
2018-05-22 15:40:22 +0800

    update

--- src/components/HelloWorld2.vue
+++ src/components/HelloWorld2.vue
@@ -1,28 +1,30 @@
 <template>
     <div class="borad container">
       <div class="input-group my-5">
+        <input type="text" placeholder="輸入暱稱" v-model="username">
         <input type="text" class="form-control" placeholder="請輸入留言內容" v-model="message">
         <div class="input-group-append">
-        <button class="btn btn-outline-primary" @click="setMessage" type="button">Enter</button>
+        <button class="btn btn-outline-primary" @click="sendMessage" type="button">Enter</button>
         </div>
       </div>
       <div class="media my-3" v-for="text in messages" >
-        <img v-if="text.username != username" class="align-self-start mr-3" src="https://picsum.photos/80/100">
+        <img v-if="text.username != username" class="align-self-start mr-3" src="http://lorempixel.com/80/100">
         <div class="media-body">
           <h3>{{text.username}}</h3>
           <p>{{text.message}}</p>
         </div>
-        <img v-if="text.username == username" class="align-self-start mr-3" src="https://picsum.photos/80/100?image=0">
+        <img v-if="text.username == username" class="align-self-start mr-3" src="http://lorempixel.com/80/100">
       </div>
   </div>
 </template>
 
 <script>
+var messagesRef = firebase.database().ref('/messages/')
 export default {
   data () {
     return {
-      username: 'VueCli',
       message: '',
+      username: '',
       messages: [
         {
           username: 'VueCli',
@@ -35,13 +37,27 @@ export default {
       ]
     }
   },
+  mounted () {
+    let vm = this;
+    let messages = [];
+      messagesRef.orderByChild("timestamp").on("value",function(snapshot){
+        snapshot.forEach(function(msgval){
+          messages.unshift(msgval.val());
+        })
+      });
+      vm.messages = messages
+  },
   methods:{
-    setMessage(){
-      let vm = this
-      vm.messages.unshift({
+    sendMessage () {
+      let vm = this;
+      let timestamp = Math.floor(Date.now() / 1000);
+      messagesRef.child(timestamp).set({
+        timestamp: timestamp,
         username: vm.username,
         message: vm.message,
       })
+      vm.message = '';
+      console.log(messagesRef)
     }
   },
 };
